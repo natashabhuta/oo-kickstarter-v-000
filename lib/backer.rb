0bf0dd63997ad6a3adbc9567f4d83d8db373282b
natashabhuta <natasha.bhuta@gmail.com>
2018-08-31 20:58:04 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :backed_projects
     @backed_projects = []
     
   def back_project(project)
-    @backed_projects >> project
+    @backed_projects << project
   end 
 end 
 end 
